eeea007a9ab4eda156b8d96eee501ff420f059a2
2213zzb <2480425871@qq.com>
2025-05-27 22:19:18 +0800

    zzb

--- main.cpp
+++ main.cpp
@@ -26,6 +26,7 @@ void processUav(int uavId, std::unordered_map<int, std::vector<Target>>& uavs, s
         //对于每个检测到的目标，将其与现有目标进行比较
         //找到最近的目标比较距离，如果小于阈值则更新，否则就添加到unmatch队列
         const double DIST_THRESHOLD = 0.0005; // 距离阈值
+        const double VELO_COEFFICIENT = 1e-6; // 速度系数
 
         while (!newTargets.empty() {
             Target newTarget = newTargets.front();
@@ -33,19 +34,40 @@ void processUav(int uavId, std::unordered_map<int, std::vector<Target>>& uavs, s
             std::lock_guard<std::mutex> lock(mtx); // 保证线程安全
             double minDist = std::numeric_limits<double>::max();
             int minIdx = -1;
+            int minType = -1; //距离最小的目标的类别：0-暂存组，1-全局数据
+
+            
             for (size_t i = 0; i < unmatchedTargets.size(); ++i) {
                 double dist = std::sqrt(
                     (unmatchedTargets[i].latitude - newTarget.latitude) * (unmatchedTargets[i].latitude - newTarget.latitude) +
                     (unmatchedTargets[i].longitude - newTarget.longitude) * (unmatchedTargets[i].longitude - newTarget.longitude)
-                );
+                ) + VELO_COEFFICIENT * (newTarget.timestamp - unmatchedTargets[i].timestamp);
                 if (dist < minDist) {
                     minDist = dist;
                     minIdx = static_cast<int>(i);
+                    minType = 0;
                 }
             }
-            if (minIdx != -1 && minDist < DIST_THRESHOLD) {
+            for (size_t i = 0; i < uavs[uavId].size(); ++i) {
+                double dist = std::sqrt(
+                    (uavs[uavId][i].latitude - newTarget.latitude) * (uavs[uavId][i].latitude - newTarget.latitude) +
+                    (uavs[uavId][i].longitude - newTarget.longitude) * (uavs[uavId][i].longitude - newTarget.longitude)
+                ) + VELO_COEFFICIENT * (newTarget.timestamp - uavs[uavId][i].timestamp);
+                if (dist < minDist) {
+                    minDist = dist;
+                    minIdx = static_cast<int>(i);
+                    minType = 1;
+                }
+            }
+
+
+            if (minType == 0 && minDist < DIST_THRESHOLD) {
                 // 距离小于阈值，更新目标
                 updateTarget(unmatchedTargets[minIdx], newTarget.timestamp, newTarget.latitude, newTarget.longitude);
+            }
+            else if (minType == 1 && minDist < DIST_THRESHOLD) {
+                // 距离小于阈值，更新目标
+                updateTarget(uavs[uavId][minIdx], newTarget.timestamp, newTarget.latitude, newTarget.longitude);
             }
             else {
                 // 没有匹配到，加入unmatchedTargets队列
